4b364acb83dbf0cc935f80ae615787a74c27bf00
Jacob <jacobpayne007@gmail.com>
2025-04-03 04:23:56 -0500

    final CORS touches

--- server/__init__.py
+++ server/__init__.py
@@ -19,11 +19,12 @@ def create_app(test_config=None):
         template_folder='../client/build')
     # app.config.from_mapping(
     # )
-    CORS(app, supports_credentials=True, origins=["https://jpayne07.github.io/"])
+    CORS(app, supports_credentials=True, origins=["https://jpayne07.github.io/"],allow_headers=["Content-Type", "Authorization", "X-Requested-With"])
     @app.after_request
     def add_cors_headers(response):
         response.headers['Access-Control-Allow-Origin'] = 'https://jpayne07.github.io'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
+        
         return response
     if env == 'BANK_ENV':
         # load the instance config, if it exists, when not testing
