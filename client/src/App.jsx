8ad11c94f0ad6481b9cfc6d43a6b3e7d90b3990a
Jacob <jacobpayne007@gmail.com>
2025-04-06 16:10:38 -0500

    update all links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,9 +25,9 @@ function App() {
       {/* Main Content */}
       <main className="flex-1 overflow-auto">
       <nav className='navbar font-body font-semibold text-xl bg-gray text-white'>
-        <p className='px-3 md:px-5'>Main</p>
-        <p className='px-3 md:px-5'>Projects</p>
-        <p className='px-3 md:px-5'>Contact</p>
+        <a href='/#skills'><p className='px-3 md:px-5'>Skills</p></a>
+        <a href='/#projects'><p className='px-3 md:px-5'>Projects</p></a>
+        <a href='/#contact'><p className='px-3 md:px-5'>Contact</p></a>
       </nav>
         {/* Hero Section */}
         <section className="gap-x-20 gap-y-10 
@@ -87,7 +87,7 @@ function App() {
           </div>
           
         </section>
-        <section id="skills"className='bg-beige my-20 rounded-lg md:p-20 p-10'>
+        <section id="skills"className='bg-beige my-10 rounded-lg md:p-20 p-10'>
           <div className='grid grid-cols-1 md:grid-cols-2 auto-rows-min'>
             <h2 className='font-header text-4xl 
             text-plum md:text-left text-center '>Overview</h2>
@@ -117,8 +117,8 @@ function App() {
          
         </section>
         {/* Projects Section */}
-        <section id="projects" className="py-16 xl:px-24 bg-gray-100">
-          <h2 className="text-2xl font-bold mb-8 text-center text-gray">Featured Projects</h2>
+        <section id="projects" className=" xl:px-24 bg-gray-100">
+          <h2 className="text-4xl font-bold mb-8 text-center text-gray py-10">Featured Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="bg-beige p-6 rounded-lg shadow-md">
             <h3 className="text-3xl md:text-2xl font-heading font-bold mb-2 text-gray md:text-left text-center pt-10">nGram Explorer</h3>
@@ -136,16 +136,23 @@ function App() {
                 A word analyzer tool used to understand word frequencies and weights.
               </p>
               <div className="mt-6 flex flex-col sm:flex-row gap-4">
-              <button
+              <a
                 href="/ngram_analysis/"
-                className="flex-1 text-center md:text-sm bg-plum text-white py-2 px-4 rounded-lg font-body shadow">
+                className="flex items-center justify-center flex-1 text-sm md:text-sm bg-plum text-white py-2 px-4 rounded-lg font-body shadow"
+>
                 View Demo
-              </button>
-              <button
-                href="/ngram_analysis/"
-                className="flex-1 text-center md:text-sm bg-beige border-plum border-solid border-5 text-plum  py-2 px-4 rounded-lg font-body shadow">
+         
+              </a>
+              
+              <a 
+                href="https://github.com/Jpayne07/ngram_analysis"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex-1 text-center md:text-sm bg-beige border-plum border-4 border-solid text-plum py-2 px-4 rounded-lg font-body shadow inline-block"
+              >
                 Source
-              </button>
+              </a>
+                            
               </div>
             </div>
             <div className="bg-gray p-6 rounded-lg shadow-md">
@@ -164,17 +171,21 @@ function App() {
                 A lightweight budgeting app to help users understand and vizualize their finances.
               </p>
               <div className="mt-6 flex flex-col sm:flex-row gap-4">
-              <button
-                href="/ngram_analysis/"
-                className="flex-1 text-center md:text-sm bg-plum text-white py-2 px-4 rounded-lg font-body shadow">
+              <a
+                href="/Fullstack_Portfolio_Banking_app/login"
+                className="flex items-center justify-center flex-1 text-sm md:text-sm bg-plum text-white py-2 px-4 rounded-lg font-body shadow"
+>
                 View Demo
-              </button>
-              <button
-                href="/ngram_analysis/"
-                className="flex-1 text-center md:text-sm bg-beige border-plum border-solid border-5 text-plum py-2 px-4 rounded-lg font-body shadow">
+              </a>
               
+              <a 
+                href="https://github.com/Jpayne07/Fullstack_Portfolio_Banking_app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex-1 text-center md:text-sm bg-beige border-plum border-4 border-solid text-plum py-2 px-4 rounded-lg font-body shadow inline-block"
+              >
                 Source
-              </button>
+              </a>
               </div>
             </div>
             {/* <div className="bg-beige p-6 rounded-lg shadow-md">
@@ -211,9 +222,9 @@ function App() {
         
 
         {/* Contact Section */}
-        <section id="contact" className="py-16 px-8 md:px-16 lg:px-24 bg-white">
-          <h2 className="text-2xl font-bold mb-4 text-center">Let's Connect</h2>
-          <p className="text-center text-gray-600">Email: jacob@example.com</p>
+        <section id="contact" className="py-16 px-8 md:px-16 lg:px-24 bg-white  mt-20">
+          <h2 className="text-2xl font-bold mb-4 text-center text-gray">Let's Connect</h2>
+          <a href='mailto:jacobpayne007@gmail.com'><p className="text-center text-gray-600">Email: jacobpayne007@gmail.com</p></a>
         </section>
       </main>
     </div>
