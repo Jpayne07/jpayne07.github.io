b1383b652aca81d0bb8815b96f51dfd14a04d363
Jacob <jacobpayne007@gmail.com>
2025-04-07 08:39:04 -0500

    update config

--- server/__init__.py
+++ server/__init__.py
@@ -19,14 +19,7 @@ def create_app(test_config=None):
         template_folder='../client/build')
     # app.config.from_mapping(
     # )
-    CORS(app, supports_credentials=True, origins=["https://jpayne07.github.io/"],allow_headers=["Content-Type", "Authorization", "X-Requested-With"])
-    @app.after_request
-    def add_cors_headers(response):
-        response.headers['Access-Control-Allow-Origin'] = 'https://jpayne07.github.io'
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization,X-Requested-With'
-        
-        return response
+    CORS(app, supports_credentials=True, origins=["https://jpayne07.github.io/", "http://localhost:3000/"],allow_headers=["Content-Type", "Authorization", "X-Requested-With"])
     if env == 'BANK_ENV':
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
